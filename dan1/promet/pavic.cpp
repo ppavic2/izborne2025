3fb9ea392f734d4f90e05f1c30143f9176e08e7f
Patrick Pavic <patrik.pavic2@gmail.com>
2025-05-02 10:18:19 +0200

    a

--- dan1/promet/pavic.cpp
+++ dan1/promet/pavic.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const int N = 55;
+const int N = 105;
 
 int n, MOD;
 
@@ -29,6 +29,9 @@ inline int pot(int A, int B) {
 	}
 	return ret;
 }
+
+int dp[N][N][N][N];
+int dp2[N][N][N];
 
 
 int pot2[N * N], fak[N], ch[N][N];
@@ -70,15 +73,47 @@ int f2(int cvor, int zadnji, int nula) {
 int ans[N];
 
 int main() {
-	//scanf("%d%d", &n, &MOD);
-	scanf("%d", &n); MOD = 998244353;
+	scanf("%d%d", &n, &MOD);
 	precompute();
+	
+	for(int PLJ_L = 1;PLJ_L <= n;PLJ_L++) dp[n - 1][PLJ_L][1][0] = 1;
+	
+	for(int zadnji = 1;zadnji <= n;zadnji++) {
+		for(int cvor = zadnji;cvor >= 1;cvor--) {
+			for(int nula = 0;nula <= cvor;nula++) {
+				int &ret = dp2[cvor][zadnji][nula];
+				if(cvor == zadnji) ret = pot2[cvor - nula];
+				else {
+					int poz = cvor - nula;
+					for(int k = 0;k <= nula;k++) {
+						ret = add(ret, mul(dp2[cvor + 1][zadnji][nula - k + 1], mul(ch[nula][k], sub(pot2[poz], !k))));
+					}
+				}
+			}
+		}
+	}
+	
+	for(int cvor = n - 2;cvor >= 1;cvor--) {
+		for(int J_LJ = 1;J_LJ <= n - cvor;J_LJ++) {
+			for(int J_C = 0;J_C <= n - cvor - 1;J_C++) {
+				for(int PLJ_L = 1;PLJ_L <= cvor;PLJ_L++) {
+					int ZC_L = cvor - PLJ_L, &ret = dp[cvor][PLJ_L][J_LJ][J_C];
+					ret = add(ret, mul(dp[cvor + 1][PLJ_L + 1][J_LJ][J_C], mul(pot2[ZC_L], sub(pot2[PLJ_L], 1))));
+					ret = add(ret, mul(dp[cvor + 1][PLJ_L][J_LJ][J_C], pot2[ZC_L]));
+					if(J_C > 0)
+						ret = add(ret, mul(dp[cvor + 1][PLJ_L][J_LJ][J_C - 1], sub(pot2[J_LJ + J_C - 1], 1)));
+					ret = add(ret, dp[cvor + 1][PLJ_L + 1][J_LJ - 1][J_C]);
+				}
+			}
+		}
+	}
+	
 	int sve = pot2[n * (n - 1) / 2];
 	for(int i = 2;i <= n;i++) {
 		for(int c = 0;c + i <= n;c++) {
-			ans[i] = add(ans[i], f(1, 1, i - 1, c));
+			ans[i] = add(ans[i], dp[1][1][i - 1][c]);
 		}
-		ans[i] = mul(ans[i], f2(1, i - 1, 1));
+		ans[i] = mul(ans[i], dp2[1][i - 1][1]);
 		sve = sub(sve, ans[i]);
 	}
 	printf("%d %d ", sve, 0);
